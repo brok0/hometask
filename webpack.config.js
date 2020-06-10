ae0c942248445bb8372e9792e5b0c6cf0d3ca093
Boris Hlynsky <bglinskii6@gmail.com>
2020-06-10 13:04:28 +0300

    asset setup

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+"use strict";
+
+module.exports = {
+  mode: "development",
+  entry: "./index",
+  output: {
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+};
